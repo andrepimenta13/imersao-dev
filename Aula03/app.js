83ec386041797f71f4d54d2df3303048e5685c12
Andre Pimenta <trz.andre@gmail.com>
2022-03-09 21:05:16 +0000

    :bug: Quick fix to show result in a h2 and delete alert message and add new button to reset
    Quick fix to show result in a h2 and delete alert message and add new button to reset

--- Aula03/app.js
+++ Aula03/app.js
@@ -1,38 +1,45 @@
-let numeroSecreto = Math.floor(Math.random() * 11);
+let numeroSecreto = Math.floor(Math.random() * 11); //podiamos fazer de outra forma: parseInt(Math.random() * 11)
 let tentativasMax = 5;
 let tentativas = 0;
 
 const Chutar = () => {
     let chute = parseInt(document.getElementById('valor').value);
     let element = document.getElementById('valor').value;
+    let resultado = document.getElementById('resultado');
 
     if (chute < 0 || chute > 10 || element === '') {
-        alert('Insira um número de 0 a 10, por favor!')
+        resultado.innerHTML = ('Insira um número de 0 a 10, por favor!');
         document.getElementById('valor').value = "";
 
     } else if (chute === numeroSecreto) {
         tentativas++;
-        alert(`Esse é o número secreto! Parabéns, você acertou na tentativa número ${tentativas}!`)
+        resultado.innerHTML = (`Parabéns, ${numeroSecreto} é o número secreto! Você acertou na tentativa número ${tentativas}!`)
         tentativas = 0;
         document.getElementById('valor').value = "";
         numeroSecreto = Math.floor(Math.random() * 11);
 
     } else if (chute > numeroSecreto) {
         tentativas++;
-        alert(`Não é esse o número secreto! O número secreto é menor. Esta é a sua tentativa número ${tentativas}!`)
+        resultado.innerHTML = (`Não é esse o número secreto! O número secreto é menor. Esta é a sua tentativa número ${tentativas}!`)
         document.getElementById('valor').value = "";
 
     } else if (chute < numeroSecreto) {
         tentativas++
-        alert(`Não é esse o número secreto! O número é maior. Esta é a sua tentativa número ${tentativas}!`)
+        resultado.innerHTML = (`Não é esse o número secreto! O número é maior. Esta é a sua tentativa número ${tentativas}!`)
         document.getElementById('valor').value = "";
     }
 
     if (tentativas >= tentativasMax) {
-        alert(`Chegou ao limite de ${tentativas} tentativas. O número secreto é 3!`);
+        resultado.innerHTML = (`Chegou ao limite de ${tentativas} tentativas. O número secreto é ${numeroSecreto}!`);
         tentativas = 0;
         document.getElementById('valor').value = "";
         numeroSecreto = Math.floor(Math.random() * 11);
     }
-    console.log(numeroSecreto)
+}
+
+const Reset = () => {
+    tentativas = 0;
+    document.getElementById('valor').value = "";
+    numeroSecreto = Math.floor(Math.random() * 11);
+    resultado.innerHTML = ('')
 }
