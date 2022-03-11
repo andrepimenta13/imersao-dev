090e82c26974b3c734de3bb202658df75cfbeada
Andre Pimenta <trz.andre@gmail.com>
2022-03-11 15:52:47 +0000

    add more functions: Listar and Remove

--- Aula05/script.js
+++ Aula05/script.js
@@ -1,14 +1,47 @@
 /*
  * TODO:
- * - [] Criar um botão para remover um filme na tela
  * - [] Além de colocar a imagem do filme, também adicionar o nome por meio de outro input
  * - [] Guardar todos os filmes adicionados em uma lista/array e percorrer essa lista toda vez que quiser imprimir ou remover o filme
- * - [] Conversor de moedas: criar funções para cada tipo de conversão e chamá-las dependendo do que você quiser fazer, podendo colocar inputs ou botões e quando clicar chamar cada função converteDolar(), converteReal(), converteEuro() e converteBitcoin(), por exemplo
  */
 
 const adicionarFilme = () => {
 
-    let campoFilmeFavorito = document.getElementById('filme').value;
-    console.log(campoFilmeFavorito);
+    let filmeFavorito = document.getElementById('filme').value;
+    let nomeFilme = document.getElementById('nomeFilme').value;
+
+    if (nomeFilme == '' || filmeFavorito == '') {
+        console.error('Deve preencher o endereço da imagem e o seu nome!');
+        alert('Deve preencher o endereço da imagem e o seu nome!');
+        return
+    }
+
+    if (filmeFavorito.endsWith(".jpg") || filmeFavorito.endsWith(".jpeg")) { //validar se a string termina com .jpeg
+        listarFilmesNaTela(filmeFavorito, nomeFilme);
+    } else {
+        console.error('Endereço de filme inválido!');
+        alert('Endereço de filme inválido!');
+    }
     document.getElementById('filme').value = '';
+    document.getElementById('nomeFilme').value = '';
+}
+
+
+const listarFilmesNaTela = (filme, nomeFilme) => {
+    let elementoFilmeFavorito = "<img src=" + filme + ">";
+    let elementoNomeFilme = "<p>" + nomeFilme + "</p>"
+    let elementoListaFilmes = document.getElementById('listaFilmes');
+
+    elementoListaFilmes.innerHTML = elementoListaFilmes.innerHTML + elementoFilmeFavorito + elementoNomeFilme; //adicionar filmes
+
+}
+
+/* 
+* TODO:
+ * - [] Criar um botão para remover um filme na tela através do mesmo endereço;
+
+*/
+
+const removerFilme = () => {
+    let elementoListaFilmes = document.getElementById('listaFilmes');
+    elementoListaFilmes.removeChild(elementoListaFilmes.lastElementChild);
 }
