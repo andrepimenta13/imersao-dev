ea3064d02705bc42e67f4ac1e6cb9bb5b4b948e0
Andre Pimenta <trz.andre@gmail.com>
2022-03-11 19:25:07 +0000

    :sparkles: Add more checks, implement arrays to all functionalities"
    Add more checks, implement arrays to all functionalities"

--- Aula05/script.js
+++ Aula05/script.js
@@ -2,20 +2,25 @@
  * TODO:
  * - [] Além de colocar a imagem do filme, também adicionar o nome por meio de outro input
  * - [] Guardar todos os filmes adicionados em uma lista/array e percorrer essa lista toda vez que quiser imprimir ou remover o filme
+ * - [] Criar um botão para remover um filme na tela através do mesmo endereço;
+
  */
+
+let moviesArray = [];
+let moviesNames = [];
 
 const adicionarFilme = () => {
 
     let filmeFavorito = document.getElementById('filme').value;
     let nomeFilme = document.getElementById('nomeFilme').value;
 
-    if (nomeFilme == '' || filmeFavorito == '') {
+    if (moviesArray.includes(filmeFavorito)) {
+        alert('Este filme é repetido!');
+        document.getElementById('filme').value = "";
+    } else if (nomeFilme == '' || filmeFavorito == '') {
         console.error('Deve preencher o endereço da imagem e o seu nome!');
         alert('Deve preencher o endereço da imagem e o seu nome!');
-        return
-    }
-
-    if (filmeFavorito.endsWith(".jpg") || filmeFavorito.endsWith(".jpeg")) { //validar se a string termina com .jpeg
+    } else if (filmeFavorito.endsWith(".jpg") || filmeFavorito.endsWith(".jpeg")) { //validar se a string termina com .jpeg
         listarFilmesNaTela(filmeFavorito, nomeFilme);
     } else {
         console.error('Endereço de filme inválido!');
@@ -25,23 +30,27 @@ const adicionarFilme = () => {
     document.getElementById('nomeFilme').value = '';
 }
 
-
 const listarFilmesNaTela = (filme, nomeFilme) => {
-    let elementoFilmeFavorito = "<img src=" + filme + ">";
-    let elementoNomeFilme = "<p>" + nomeFilme + "</p>"
-    let elementoListaFilmes = document.getElementById('listaFilmes');
-
-    elementoListaFilmes.innerHTML = elementoListaFilmes.innerHTML + elementoFilmeFavorito + elementoNomeFilme; //adicionar filmes
-
+    moviesArray.push(filme);
+    moviesNames.push(nomeFilme);
+    const image = document.createElement("img");
+    const name = document.createElement('label');
+    image.setAttribute('src', filme);
+    name.innerHTML = nomeFilme;
+    const movieContainer = document.getElementById('listaFilmes');
+    movieContainer.appendChild(image);
+    movieContainer.appendChild(name);
 }
-
-/* 
-* TODO:
- * - [] Criar um botão para remover um filme na tela através do mesmo endereço;
-
-*/
 
 const removerFilme = () => {
     let elementoListaFilmes = document.getElementById('listaFilmes');
-    elementoListaFilmes.removeChild(elementoListaFilmes.lastElementChild);
+
+    if (elementoListaFilmes.hasChildNodes()) {
+        elementoListaFilmes.removeChild(elementoListaFilmes.lastElementChild);
+        elementoListaFilmes.removeChild(elementoListaFilmes.lastElementChild); //que trolhice, melhora isso!
+        moviesArray.pop();
+        moviesNames.pop();
+    } else {
+        alert('Não existem mais filmes para apagar!')
+    }
 }
