a9fab783a29ac52e7078e7bc5227265bccacc7c7
andrepimenta13 <trz.andre@gmail.com>
2022-03-28 15:44:56 +0100

    add functionalities to class 07

--- Aula07/scripts.js
+++ Aula07/scripts.js
@@ -32,14 +32,55 @@ var cartaJogador;
 
 function sortearCarta() {
 
-    let numeroCartaMaquina = cartas[Math.floor(Math.random() * cartas.length)];
+    cartaMaquina = cartas[Math.floor(Math.random() * cartas.length)];
 
-    let numeroCartaJogador = cartas[Math.floor(Math.random() * cartas.length)];
+    cartaJogador = cartas[Math.floor(Math.random() * cartas.length)];
 
-    while (numeroCartaMaquina == numeroCartaJogador) {
-        numeroCartaJogador = cartas[Math.floor(Math.random() * cartas.length)];
+    while (cartaMaquina == cartaJogador) {
+        cartaJogador = cartas[Math.floor(Math.random() * cartas.length)];
     }
-    //document.getElementById('btnSortear');
+
+    document.getElementById('btnSortear').disabled = true;
+    document.getElementById('btnJogar').disabled = false;
+
+    exibirOpcoes(cartaJogador);
 }
 
-//40:33
+function exibirOpcoes(cartaJogador) {
+    let opcoes = document.getElementById('opcoes');
+
+    let opcoesTexto = "";
+
+    for (var atributo in cartaJogador.atributos) {
+        opcoesTexto += "<input type = 'radio' name = 'atributo' value = '" + atributo + "'>" + atributo;
+    }
+    opcoes.innerHTML = opcoesTexto;
+}
+
+function obtemAtributoSelecionado() {
+    var radioAtributos = document.getElementsByName("atributo");
+
+    for (var index = 0; index < radioAtributos.length; index++) {
+
+        if (radioAtributos[index].checked == true) {
+            return radioAtributos[index].value;
+        }
+    }
+}
+
+function jogar() {
+    let atributoSelecionado = obtemAtributoSelecionado();
+    let elementoResultado = document.getElementById('resultado');
+
+    let valorCartaJogador = cartaJogador.atributos[atributoSelecionado];
+    let valorCartaMaquina = cartaMaquina.atributos[atributoSelecionado];
+
+    if (valorCartaJogador > valorCartaMaquina) {
+        elementoResultado.innerHTML = 'Você venceu!';
+    } else if (valorCartaJogador < valorCartaMaquina) {
+        elementoResultado.innerHTML = "Você perdeu!";
+    } else {
+        elementoResultado.innerHTML = "Empatou!";
+    }
+
+}
